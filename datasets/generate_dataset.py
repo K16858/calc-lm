3fa32c579f32ae0a955048192ac738f1d26d6735
Kosuke T <ktohda0127@gmail.com>
2025-11-13 00:21:01 +0900

    シンプルな四則演算を行う関数を追加

---
+++ datasets/generate_dataset.py
@@ -0,0 +1,26 @@
+import random
+
+def generate_math(max_value=100):
+    a = random.randint(1, max_value)
+    b = random.randint(1, max_value)
+    op = random.choice(["+", "-", "*", "/"])
+
+    result = 9
+    if op == "+":
+        result = a + b
+    elif op == "-":
+        result = a - b
+    elif op == "*":
+        result = a * b
+    elif op == "/":
+        if b == 0:
+            result = "undefined"
+        else:
+            result = a / b
+
+    return f"{a} {op} {b} = {result}"
+
+
+if __name__ == "__main__":
+    for _ in range(10):
+        print(generate_math(50))
