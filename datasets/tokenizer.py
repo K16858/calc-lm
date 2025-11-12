a3d87a53982a82fc1469d4b6ca28394cb1d95e5e
Kosuke T <ktohda0127@gmail.com>
2025-11-13 00:42:17 +0900

    エンコードデコードメソッドを追加

--- datasets/tokenizer.py
+++ datasets/tokenizer.py
@@ -42,9 +42,33 @@ class Tokenizer:
         
         return tokens
     
+    def encode(self, text: str, add_special_tokens: bool = True) -> List[int]:
+        tokens = self.tokenize(text)
+        
+        if add_special_tokens:
+            tokens = [self.sos_token] + tokens + [self.eos_token]
+        
+        token_ids = [self.token2id.get(token, self.unk_id) for token in tokens]
+        return token_ids
+    
+    def decode(self, token_ids: List[int], skip_special_tokens: bool = True) -> str:
+        tokens = [self.id2token.get(idx, self.unk_token) for idx in token_ids]
+        
+        if skip_special_tokens:
+            tokens = [
+                t for t in tokens 
+                if t not in [self.pad_token, self.sos_token, self.eos_token]
+            ]
+        
+        return "".join(tokens)
+    
 if __name__ == "__main__":
     tokenizer = Tokenizer()
     sample_text = "12 + 34 = 46"
     tokens = tokenizer.tokenize(sample_text)
     print("Text:", sample_text)
     print("Tokens:", tokens)
+    token_ids = tokenizer.encode(sample_text)
+    print("Token IDs:", token_ids)
+    decoded_text = tokenizer.decode(token_ids)
+    print("Decoded Text:", decoded_text)
